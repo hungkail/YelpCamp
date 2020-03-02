8eb5477ce81790ff50798f65e74358da72de7146
Hung Kai Liao <hungkail@bekeley.edu>
2020-03-02 01:33:05 -0800

    make login, sign up, logout button work

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.get("/campgrounds/:id", function (req, res) {
 // COMMENTS ROUTES
 // ===================
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new",isLoggedIn, function (req, res) {
     // find campground by id
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
@@ -102,7 +102,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments",isLoggedIn, function (req, res) {
     //lookup campground using ID
     Campground.findById(req.params.id, function (err, foundCampground) {
         if (err) {
@@ -166,5 +166,12 @@ app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/campgrounds");
 });
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port, () => console.log(`YelpCamp listening on port ${port}`));
