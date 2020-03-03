6050b0d17340d160c0c4c1de2ac6b84d27c0dcab
Hung Kai Liao <hungkail@bekeley.edu>
2020-03-03 00:06:31 -0800

    add dynamic price to each post

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,7 @@ mongoose.set('useFindAndModify', false);
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author: {
